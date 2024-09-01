2891f7764a6f418adc6227bfc747df0888bfa9a5
KadenStigter <kstigter@mail.bradley.edu>
2024-09-01 17:38:58 -0500

    removed uneeded assets

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -15,8 +15,6 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] private float _spawnRate = 5.0f;
     [SerializeField] private GameObject[] _enemies;
     [SerializeField] private bool _isSpawning = true;
-    private float spawnX = 7.0f;
-    private float[] spawnY;
 
     /// <summary>
     /// Start is called before the first frame update
@@ -24,8 +22,6 @@ public class EnemySpawner : MonoBehaviour
     void Start()
     {
         StartCoroutine(Spawner());
-        spawnY = new float[5];
-        spawnY = new float[] { 0.0f, 1.0f, 2.0f, 3.0f, 4.0f };
     }
 
     /// <summary>
@@ -40,11 +36,8 @@ public class EnemySpawner : MonoBehaviour
             yield return wait;
             int rand = Random.Range(0, _enemies.Length);
             GameObject enemyToSpawn = _enemies[rand];
-            Vector3 newPosObs = new Vector3();
-            newPosObs.x = spawnX;
-            int randPos = Random.Range(0, spawnY.Length);
-            newPosObs.y = spawnY[randPos];
-            Instantiate(enemyToSpawn, newPosObs, Quaternion.identity);
+
+            Instantiate(enemyToSpawn, transform.position, Quaternion.identity);
         }
     }
 }
